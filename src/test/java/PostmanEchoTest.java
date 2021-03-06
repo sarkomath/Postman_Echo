ce06c3cd27bdeb994d4bdf5560f7e04870250fa7
Nikita Novoselov <sarkomath@outlook.com>
2021-03-06 16:48:18 +1000

    Initial commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void mustPOST() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("any") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("any"));
+    }
+}
